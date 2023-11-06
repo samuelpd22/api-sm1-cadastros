a93813928cd4c8a83b77e370fd2d190d11bf4d88
samuelpd22 <samuel050101@hotmail.com>
2023-11-06 19:13:22 -0300

    Implementando filter entre o request e response da api

--- src/main/java/com/apicadastro/apicadastro/security/SecurityConfig.java
+++ src/main/java/com/apicadastro/apicadastro/security/SecurityConfig.java
@@ -1,24 +1,30 @@
 package com.apicadastro.apicadastro.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
+@Bean
 public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
     return http
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers(HttpMethod.GET, "/usuarios").hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.GET, "/usuarios").permitAll()
                     .anyRequest().authenticated()
-            )
+
+
+            ).addFilterBefore(new SecurityFilter(), UsernamePasswordAuthenticationFilter.class)
             .build();
+
+
     }
 }
